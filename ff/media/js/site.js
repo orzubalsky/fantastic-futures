1d2d8cec9227ae632ab7c290664cf85054444697
Or Zubalsky <juviley@gmail.com>
2012-08-22 21:36:13 -0400

    removed unused js files

--- ff/media/js/site.js
+++ ff/media/js/site.js
@@ -2,9 +2,12 @@
 	var site = window.site = new function() {
 	    this.WIDTH;
 	    this.HEIGHT;
-	    
+	    this.csrvToken;
 		this.init = function() 
 		{
+            // Dajaxice.futures.sayhello(function(data) { lib.log(data); });         
+		    
+            this.csrvToken = $('input[name="csrfmiddlewaretoken"]').val();		    
 		    this.menus();
 		    this.WIDTH  = $('#interface').width();
 		    this.HEIGHT = $('#interface').height();
@@ -13,6 +16,8 @@
 		
 		this.menus = function()
 		{
+		    var self = this;
+		    
         	$("#logo").click(function() {
         	  $("#about").fadeToggle("fast", "linear");
         	});
@@ -49,6 +54,54 @@
         	        }, 1000
         	    );
         	});
+        	$('#feedbackForm').submit(function(e)
+        	{
+        	    e.preventDefault();
+        	    
+                var data = $(this).serialize();
+                Dajaxice.futures.submit_feedback(self.feedback_callback, {'form':data});
+        	});
+		};
+		
+		this.feedback_callback = function(data)
+		{
+		    // reset error fields
+		    $('#feedbackForm input, #feedbackForm textarea').removeClass('error');
+		    
+            if (data.success == true)
+            {
+                // 1. fade out form
+                $('#feedbackForm').fadeOut(300, function() 
+                {
+                    // 2. fade in success message
+                    $('#feedbackCheck').fadeIn(300, function() 
+                    {
+                        // 3. wait 1000 ms
+                        setTimeout(function() 
+                        {
+                            // 4. animate form back to its original position
+                            $('#feedback').animate(
+                    	        {
+                    	            right: -322
+                    	        }, 1000, function() 
+                    	            {
+                    	                // 5. restore the form's original state
+                    	                $('#feedbackCheck').hide();
+                    	                $('#feedbackForm input, #feedbackForm textarea').not('.formSubmit').val('');
+                    	                $('#feedbackForm').show();
+                    	            }
+                    	    );
+                        }, 1000);
+                    });
+                });
+            }
+            else
+            {
+                for (field in data.errors)
+                {
+                    $('#id_' + field).addClass('error');
+                }
+            }
 		};
 	};
 })(jQuery);
