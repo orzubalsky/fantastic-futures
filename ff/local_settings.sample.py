3ae81eb2c075799b9fef0616cbf6576ba4450984
Or Zubalsky <juviley@gmail.com>
2012-09-14 16:35:21 -0400

    feedback form is sent as an email when submitted. closes #23

--- ff/local_settings.sample.py
+++ ff/local_settings.sample.py
@@ -49,3 +49,13 @@ LANGUAGE_CODE = 'en-us'
 
 # API keys
 GOOGLE_API_KEY = ''
+
+### for staging and development or testing:
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+### replace the above line with the following for the live site:
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST =
+# EMAIL_PORT =
+# EMAIL_HOST_USER =
+# EMAIL_HOST_PASSWORD =
+# EMAIL_USE_TLS =
