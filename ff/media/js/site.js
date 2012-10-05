87574f75dbaa6898567d4c0caf9f7cb6449937fe
Or Zubalsky <juviley@gmail.com>
2012-10-05 12:13:01 -0400

    adding javascript solution for placeholder attribute in forms. closes #87

--- ff/media/js/site.js
+++ ff/media/js/site.js
@@ -5,6 +5,15 @@
 	    this.csrvToken;
 		this.init = function() 
 		{
+		    var self = this;
+		    
+            var arrInputs = document.getElementsByTagName("input");
+            for (var i = 0; i < arrInputs.length; i++) 
+            {
+                var curInput = arrInputs[i];
+                if (!curInput.type || curInput.type == "" || curInput.type == "text")
+                self.handlePlaceholder(curInput);
+            }		    
             this.menus();
             this.constellationMenuScroll();
             this.searchForm();
@@ -385,6 +394,28 @@
                 }
             }
 		};
+		
+        this.handlePlaceholder = function(oTextbox) {
+            if (typeof oTextbox.placeholder == "undefined") {
+                var curPlaceholder = oTextbox.getAttribute("placeholder");
+                if (curPlaceholder && curPlaceholder.length > 0) {
+                    oTextbox.value = curPlaceholder;
+                    oTextbox.setAttribute("old_color", oTextbox.style.color);
+                    oTextbox.style.color = "#c0c0c0";
+                    oTextbox.onfocus = function() {
+                        this.style.color = this.getAttribute("old_color");
+                        if (this.value === curPlaceholder)
+                            this.value = "";
+                    };
+                    oTextbox.onblur = function() {
+                        if (this.value === "") {
+                            this.style.color = "#c0c0c0";
+                            this.value = curPlaceholder;
+                        }
+                    };
+                }
+            }
+        }		
 		
 		this.appendFormError = function(form, message)
 		{
