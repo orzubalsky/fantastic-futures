53de7179896b63bfcdbf5600d303938d61c2d08d
Or Zubalsky <juviley@gmail.com>
2012-10-24 14:42:24 -0400

    fixed loading constellations

--- ff/media/js/constellations.js
+++ ff/media/js/constellations.js
@@ -2,7 +2,7 @@
 	var constellations = window.constellations = new function() 
 {
     this.collection = [];
-    this.constellation;                 // current active constellation
+    this.constellation = -1;                 // current active constellation
     this.loading_constellation = false  // set to true when a constellation is loaded (rotated + zoomed)
 	this.addButton = false;             // this is used to check whether the "save constellation" appeared        
 
@@ -13,21 +13,12 @@
     };
 
     this.clear = function()
-    {
-        pov.clear();
-        connections.collection = [];
-        connections.layer.removeChildren();
-
-        if (this.constellation > 0 && !pov.is_animating)
+    {        
+        if (!this.loading_constellation)
         {
-            for (var i=0; i<CONSTELLATIONS.length; i++)
-            {
-                if (CONSTELLATIONS[i].pk == this.constellation)
-                {
-                    var constellation = CONSTELLATIONS[i].fields;
-                    this.drawOne(constellation, true, true, function() {});                        
-                }
-            }
+            pov.clear();
+            connections.collection = [];
+            connections.layer.removeChildren();
         } 
     };
 
@@ -95,13 +86,13 @@
         this.previewOne(id, true, 12, function() 
         {                                
             // after rotation/zoom is done, set loading_constellation to reflect the current state
-            this.loading_constellation = false;
-                            
+            constellations.loading_constellation = false;
+                                        
             // set active state for all connected sounds
             geosounds.setActiveStateForConnectedSounds();
             
             // reset addButton, so the loaded constellation could be altered and saved
-            this.addButton = false;
+            constellations.addButton = false;
             
             // start the player
             playhead.is_playing = true;
