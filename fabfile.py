a731f066529d5c828684f9b8c63995d7d88ff011
orzubalsky <juviley@gmail.com>
2013-08-31 15:44:13 -0400

    updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,13 +4,8 @@ from fabric.context_managers import settings
 from fabric.api import env, task, sudo, prompt, cd, put, puts
 from fab_config import Config
 
-project_dir          = Config.project_dir
-env.hosts            = Config.hosts
-fab_username         = Config.fab_username
-fab_password         = Config.fab_password
-server_settings_file = Config.server_settings_file 
-apache_conf_file     = Config.apache_conf_file
-
+env.hosts = Config.hosts
+server_settings_file = Config.server_settings_file
 
 
 #####
@@ -22,44 +17,27 @@ apache_conf_file     = Config.apache_conf_file
 def local():
     env.run = lrun
     env.hosts = ['localhost']
+    env.project_dir = Config.local_project_dir
+    env.username = Config.local_username
+    env.password = Config.local_password
 
 
 @task
 def testing():
     env.run = run
     env.hosts = ['tstest.net']
+    env.project_dir = Config.test_project_dir
+    env.username = Config.testing_username
+    env.password = Config.testing_password
 
 
 @task
 def prod():
     env.run = run
     env.hosts = ['tradeschool.coop']
-
-
-#####
-#
-# setup local machine
-#
-#####
-@task
-def setup_local():
-    # clone repository
-
-    # run buildout
-
-    # copy development.py settings file
-
-    # enter db settings
-
-    # sync database
-
-    # migrate database
-
-    # load data fixtures
-
-    # load sample data
-
-    pass
+    env.project_dir = Config.productino_project_dir
+    env.username = Config.production_username
+    env.password = Config.production_password
 
 
 #####
@@ -69,8 +47,8 @@ def setup_local():
 #####
 @task
 def update_sourcecode():
-    with cd('/opt/projects/tse/'):
-        sudo('git pull', user=fab_username)
+    with cd(env.project_dir):
+        sudo('git pull', user=env.username)
 
 
 @task
@@ -79,37 +57,36 @@ def update_project_settings():
         'Enter name of local settings file:',
         default=server_settings_file
     )
-    destination = '/opt/projects/tse/ts/settings/server.py'
+    destination = '%s/ff/settings/server.py' % env.project_dir
     put(filename, destination, use_sudo=True)
-    sudo('chown %s:webdev %s' % (fab_username, destination))
+    sudo('chown %s:webdev %s' % (env.username, destination))
 
 
 @task
 def run_buildout():
-    with cd('/opt/projects/tse/'):
-        sudo('./bin/buildout -v -c server.cfg', user=fab_username)
+    with cd(env.project_dir):
+        sudo('./bin/buildout -v -c server.cfg', user=env.username)
 
 
 @task
 def update_db():
-    with cd('/opt/projects/tse/'):
-        sudo('./bin/django syncdb', user=fab_username)
-        sudo('./bin/django migrate tradeschool', user=fab_username)
-        sudo('./bin/django migrate migration', user=fab_username)
+    with cd(env.project_dir):
+        sudo('./bin/django syncdb', user=env.username)
+        sudo('./bin/django migrate', user=env.username)
 
 
 @task
 def update_static_files():
     # run the django command to update static files
-    with cd('/opt/projects/tse/'):
-        sudo('./bin/django collectstatic', user=fab_username)
+    with cd(env.project_dir):
+        sudo('./bin/django collectstatic', user=env.username)
 
 
 @task
 def load_fixtures():
     # load fixtures
-    with cd('/opt/projects/tse/'):
-        sudo('./bin/django loaddata email_initial_data.json pages_initial_data.json teacher-info.json', user=fab_username)
+    with cd(env.project_dir):
+        sudo('./bin/django loaddata ###', user=env.username)
 
 
 @task
@@ -119,42 +96,21 @@ def restart_memcached():
 
 
 @task
-def restart_wsgi():
-    with cd('/opt/projects/tse'):
-        sudo('touch bin/django.wsgi')
+def restart_apache():
+    with cd(env.project_dir):
+        sudo('../apache/bin/restart', user=env.username)
 
 
 @task
 def restart():
     #restart_memcached()
-    restart_wsgi()
+    restart_apache()
 
 
 @task
 def test():
-    with cd('/opt/projects/tse'):
-        sudo('./bin/django test tradeschool -v 2', user=fab_username)
-
-
-@task
-def load_data():
-    filename = prompt(
-        'Enter name of sql file:',
-        default='data.sql'
-    )
-    #sudo('mkdir /opt/projects/tse/sql',user=fab_username)
-
-    destination = '/opt/projects/tse/sql/data.sql'
-
-    db_name = prompt(
-        'Enter name of database:',
-        default='tradeschool_test'
-    )
-
-    put(filename, destination, use_sudo=True)
-
-    with cd('/opt/projects/tse/sql'):
-        sudo('mysql -u root %s < data.sql' % (db_name), user=fab_username)
+    with cd(env.project_dir):
+        sudo('./bin/django test futures -v 2', user=env.username)
 
 
 @task
@@ -171,7 +127,8 @@ def deploy():
     update_sourcecode()
 
     update = prompt(
-        'Do you want to update the server settings file with a local file? (y/n)',
+        'Do you want to update the server settings file '
+        'with a local file? (y/n)',
         default='y',
         validate=r'^[yYnN]$'
     )
@@ -190,7 +147,7 @@ def deploy():
     load_fixtures()
     update_static_files()
     restart()
-    test()
+    #test()
 
 
 #####
@@ -210,86 +167,49 @@ def init_os_package_setup():
 
 
 @task
-def init_fab_user():
-    sudo('groupadd webdev')
-    sudo('useradd -G mysql,webdev --create-home --shell /bin/bash %s' % fab_username)
-    sudo('passwd %s' % fab_password)
-
-    sudo('ssh-keygen -t rsa -C "fab@tradeschool.coop"', user=fab_username)
-    puts('Created the following id_rsa.pub file for user %s:' % fab_username)
-    sudo('cat /home/%s/.ssh/id_rsa.pub' % fab_username)
-    prompt(
-        'Please upload this to github as a "deploy key"'
-        '(https://github.com/orzubalsky/tradeschool/settings/keys).\n'
-        'When done, press enter to continue.'
-    )
-
-
-@task
 def init_project_sourcecode():
-    sudo('mkdir --parents /opt/projects/tse')
-    sudo('chown %s:webdev /opt/projects/tse' % fab_username)
-    with cd('/opt/projects/tse'):
-        sudo('git clone git@github.com:orzubalsky/tradeschool.git .', user=fab_username)
+    sudo('mkdir --parents %s' % env.project_dir)
+    sudo('chown %s:webdev %s' % env.username, env.project_dir)
+    with cd(env.project_dir):
+        sudo(
+            'git clone git@github.com:orzubalsky/fantastic-futures.git .',
+            user=env.username
+        )
 
 
 @task
 def init_buildout():
-    with cd('/opt/projects/tse'):
-        sudo('python bootstrap.py -v 2.1.1 -c server.cfg', user=fab_username)
-
-
-@task
-def init_mysql_db():
-    db_name = prompt(
-        'Enter name of database:',
-        default='tradeschool_test'
-    )
-    #sudo('mysqladmin create %s -u root' % db_name, user=fab_username)
-
-    db_user = prompt(
-        'Enter name of database user:',
-        default='tradeschooler'
-    )
-    db_password = prompt('Enter password:')
-
-    sudo('mysql_install_db', user=fab_username)
-    sudo('/usr/bin/mysql_secure_installation', user=fab_username)
-
-    sudo('mysql -u root CREATE DATABASE %s;' % db_name)
-    sudo('mysql -u root CREATE USER %s@localhost IDENTIFIED BY %s;' % (db_user, db_password))
-    sudo('mysql -u root GRANT ALL PRIVILEGES ON %s.* TO %s@localhost;' % (db_name, db_user))
+    with cd(env.project_dir):
+        sudo('python bootstrap.py -v 2.1.1 -c server.cfg', user=env.username)
 
 
 @task
 def create_cache_folder():
-    with cd('/opt/projects/tse'):
-        sudo('mkdir tmp', user=fab_username)
-
-
-@task
-def init_apache():
-    filename = prompt(
-        'Enter name of local apache conf file:',
-        default=apache_conf_file
-    )
-    destination = '/etc/apache2/sites-available/tstest.net'
-    put(filename, destination, use_sudo=True)
-
-    sudo('a2ensite tstest.net')
-    sudo('service apache2 reload')  # do we need this line??
+    with cd(env.project_dir):
+        sudo('mkdir tmp', user=env.username)
 
 
 @task
 def initialize_everything():
-    init_os_package_setup()
-    init_fab_user()
     init_project_sourcecode()
     update_project_settings()
     init_buildout()
-    init_mysql_db()
 
     run_buildout()
     update_db()
 
-    init_apache()
+    restart
+
+
+#####
+#
+# setup local machine
+#
+#####
+@task
+def setup():
+
+    # sync database
+    update_db()
+
+    # load data fixtures
